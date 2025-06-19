d5af91858c9e6f03fe01e9f02f9cbee50b185dc2
João Victor Vilela dos Santos <joaovvsantos@pm.me>
2025-06-19 10:17:16 -0300

    refactor: removing unused functions

--- src/wizards/zip.code.wizard.scene.js
+++ src/wizards/zip.code.wizard.scene.js
@@ -1,4 +1,4 @@
-import { Scenes, Composer, Markup } from "telegraf";
+import { Scenes, Composer } from "telegraf";
 import searchByZipCode from "../helpers/search.by.zip.code.js";
 
 const searchZipCodeStep = new Composer();
