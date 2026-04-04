2bbd643cdb0dc60636c5562041a32c1e0cfe58df
João Victor Vilela dos Santos <joaovvsantos@proton.me>
2026-04-04 17:40:59 -0300

    ajuste em dataSchema
    Refatoração dos limites de caracteres (mínimo e máximo) na funcionalidade de pesquisa por endereço

--- src/helpers/data.validation.js
+++ src/helpers/data.validation.js
@@ -1,8 +1,8 @@
 import Joi from "joi";
 
 const dataSchema = Joi.object({
-  city: Joi.string().required().min(3).max(29),
-  street: Joi.string().required().min(3).max(51),
+  city: Joi.string().required().min(3).max(32),
+  street: Joi.string().required().min(3).max(60),
   federativeUnit: Joi.string().required().min(2).max(2),
 });
 
